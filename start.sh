ba422a01175a52173302cc9d4f0c404933dcae26
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-03 22:56:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
+git clone https://github.com/yassuhacks750/TXT-750-EXTRACTOR
 cd /TXT-EXTRACTOR
 pip install -r requirements.txt
 echo "Starting Bot..."

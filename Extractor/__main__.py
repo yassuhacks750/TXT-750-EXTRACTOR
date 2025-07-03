5b0306e8f823e222363862ce51ba4b031814d97f
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-03 11:57:37 +0530

    Add files via upload

---
+++ Extractor/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from Extractor.modules import ALL_MODULES
+
+ 
+
+loop = asyncio.get_event_loop()
+
+
+async def sumit_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("Extractor.modules." + all_module)
+
+    print("» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
+    await idle()
+    print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(sumit_boot())
+
+asyncio.run(main())
